222766677aa7ec6d31b8752282ecb56d6ad16931
Awesome Henry <henryebomah@gmail.com>
2021-04-19 07:59:19 -0700

    Update seeds.jobstar_jobs.sql

--- seeds/seeds.jobstar_jobs.sql
+++ seeds/seeds.jobstar_jobs.sql
@@ -1,9 +1,7 @@
-INSERT INTO jobs (id, company, experience, country, position, wage)
-VALUES
-  (1, 'Amazon', '3', 'USA', 'software engineer', 170000),
-  (2, 'Google', '2', 'USA', 'data scientist', 150000),
-  (3, 'Microsoft', '2', 'USA', 'software engineer', 135000),
-  (4, 'Oracle', '1', 'USA', 'app developer', 130000),
-  (5, 'Paypal', '1', 'USA', 'backend developer', 125000); 
-
-  ALTER SEQUENCE jobs RESTART WITH 12;  
+INSERT INTO jobs (id, company, experience, country, position, wage) 
+VALUES 
+(1, 'Amazon', '3', 'USA', 'software engineer', 170000),
+(2, 'Google', '2', 'USA', 'data scientist', 150000),
+(3, 'Microsoft', '2', 'USA', 'software engineer', 135000),
+(4, 'Oracle', '1', 'USA', 'app developer', 130000),
+(5, 'Paypal', '1', 'USA', 'backend developer', 125000); 
