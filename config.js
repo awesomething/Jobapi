8a1948dd3d0ea29b2eed01ce8719f8d5f3926bcc
Awesome Henry <henryebomah@gmail.com>
2021-04-19 07:54:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-/*module.exports = {
+module.exports = {
     PORT: process.env.PORT || 3003,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/vacancies'
-  }*/
+    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/applicants'
+  }
