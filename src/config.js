12f2ffff6e174706d8d8acc824272ccd844ca502
Awesome Henry <henryebomah@gmail.com>
2021-04-19 19:00:57 -0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 5000,
+    NODE_ENV: process.env.NODE_ENV || 'production',
+    DATABASE_URL: process.env.DATABASE_URL || 'mysql://b9bdca4b0b9ce1@us-cdbr-east-03.cleardb.com/clearDB_heroku'
+    
+  }
