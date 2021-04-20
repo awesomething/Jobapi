9967866ff5bc863a61e8ed97211aa20cf2825edf
Awesome Henry <henryebomah@gmail.com>
2021-04-19 19:01:14 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3003,
-    NODE_ENV: process.env.NODE_ENV || 'production',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/applicants'
-  }
