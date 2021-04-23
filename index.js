29a5e816af5895f2ad09ca82ff5c35d241472945
Awesome Henry <henryebomah@gmail.com>
2021-04-23 14:43:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const app = require('./app')
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  user: "b9bdca4b0b9ce1",
-  host: "us-cdbr-east-03.cleardb.com",
-  password: "9e85f6ec",
-  database: "heroku_2793bc343638f0e"
-});
-
 app.set('db', db)
 
 const PORT = process.env.PORT || 5000
